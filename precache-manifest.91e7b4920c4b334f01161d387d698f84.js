edfc60474449e675bf6ff3fe8e83084bba21343c
Weston Renoud <renoud@qps.nl>
2020-06-28 19:59:52 +0200

    Updates

---
+++ precache-manifest.91e7b4920c4b334f01161d387d698f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec94b31b1b56e3c4ec1f8952efc75856",
+    "url": "/idle-lines-of-code/index.html"
+  },
+  {
+    "revision": "5098a30160980c3ab6c4",
+    "url": "/idle-lines-of-code/static/css/main.9473f1e4.chunk.css"
+  },
+  {
+    "revision": "9d85124b73697679f2e0",
+    "url": "/idle-lines-of-code/static/js/2.b31e9368.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/idle-lines-of-code/static/js/2.b31e9368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5098a30160980c3ab6c4",
+    "url": "/idle-lines-of-code/static/js/main.c59210a8.chunk.js"
+  },
+  {
+    "revision": "4d0ebe7bacf1af9d94c6",
+    "url": "/idle-lines-of-code/static/js/runtime-main.71430b99.js"
+  }
+]);
