cca495187c1a95a5ef10fdf25b535da695794c9d
Weston Renoud <renoud@qps.nl>
2020-06-28 19:42:19 +0200

    added console componant for user "clicks" and basic achivement support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Console from "./Console";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      achievements: [
+        {
+          name: "A Good Days Work",
+          description: 
+            <>
+              You made 10 lines of code (LoC). In the 1975 software project
+              management book,{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://en.wikipedia.org/wiki/The_Mythical_Man-Month"
+              >
+                The Mythical Man Month: Essays on Software Engineering
+              </a>
+              , Fred Brooks states that, no matter the programming language
+              chosen, a professional developer will write an average 10 lines of
+              code (LoC) per day.
+            </>
+          ,
+          threshold: 10,
+        },
+        {
+          name: "A Better Days Work",
+          description: (
+            <>
+              You made 100 lines of code (LoC). In the Codding Horror blog post{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://blog.codinghorror.com/diseconomies-of-scale-and-lines-of-code/"
+              >
+                Diseconomies of Scale and Lines of Code
+              </a>{" "}
+              Jeff Attwood cites a table from Steve McConnell's book that gives
+              an upper bound of 25,000 LoC per year. Attwood goes on to quote
+              McConnell, "The LOC measure is a terrible way to measure software
+              size, except that all the other ways to measure size are worse."
+            </>
+          ),
+          threshold: 100,
+        },
+        {
+          name: "First code year",
+          description:
+            "You made 40 lines of code. The average programmer is said to write 10,000 lines of new code in a year. At this rate you'll be a superstar programmer in no time.",
+          threshold: 10000,
+        },
+      ],
+      upgrades: [
+        {
+          name: "Coding Camp",
+          description:
+            "You won't learn much, but at least you'll get some typing practice and a portfolio.",
+          effect: "doubles your typing speed",
+          cost: 100,
+        },
+        {
+          name: "Discover stackoverflow.com",
+          description: "Why write what you can copy.",
+          effect: "doubles your typing speed",
+          cost: 100,
+        },
+      ],
+      typingRate: 1,
+      nloc: 0,
+      lifetime_nloc: 0,
+    };
+    this.addLOC = this.addLOC.bind(this);
+  }
+
+  addLOC(loc) {
+    this.setState({ nloc: this.state.nloc + loc });
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <div>
+          <pre>
+            &nbsp;&nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;____&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;___
+            <br />
+            &nbsp;&nbsp;/&nbsp;&nbsp;_/__/&nbsp;/&nbsp;/__&nbsp;&nbsp;&nbsp;/&nbsp;_&nbsp;\_______&nbsp;&nbsp;___&nbsp;________&nbsp;___&nbsp;_&nbsp;&nbsp;__&nbsp;_&nbsp;&nbsp;___&nbsp;____
+            <br />
+            &nbsp;_/&nbsp;//&nbsp;_&nbsp;&nbsp;/&nbsp;/&nbsp;-_)&nbsp;/&nbsp;___/&nbsp;__/&nbsp;_&nbsp;\/&nbsp;_&nbsp;`/&nbsp;__/&nbsp;_&nbsp;`/&nbsp;&nbsp;'&nbsp;\/&nbsp;&nbsp;'&nbsp;\/&nbsp;-_)&nbsp;__/
+            <br />
+            /___/\_,_/_/\__/&nbsp;/_/&nbsp;&nbsp;/_/&nbsp;&nbsp;\___/\_,&nbsp;/_/&nbsp;&nbsp;\_,_/_/_/_/_/_/_/\__/_/
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/___/
+            <br />
+          </pre>
+        </div>
+        <div>
+          <h1>NLoC: {this.state.nloc}</h1>
+        </div>
+        <Console onAddLOC={this.addLOC} typingRate={this.state.typingRate} />
+        <div>
+          <h1>Upgrades</h1>
+        </div>
+        <div className="Achievements">
+          <h1>Achievements</h1>
+          <div className="AchievementList">
+            {this.state.achievements
+              .filter((achievement) => achievement.threshold < this.state.nloc)
+              .map((achievement, i) => (
+                <div className="Achievement" key={i}>
+                  <h3>{achievement.name}</h3>
+                  <p>{achievement.description}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Console.js
@@ -0,0 +1,155 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Console.css";
+
+class Console extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      character_count: 0,
+      line: 0,
+      text: [],
+      current_history: [],
+      current_prompt: "",
+      visible_history: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.focus = this.focus.bind(this);
+
+    this.selfRef = React.createRef();
+    this.promptRef = React.createRef();
+  }
+
+  computeVisibleHistory() {
+    let consoleElem = this.selfRef.current;
+
+    let clientHeight = consoleElem.clientHeight;
+    let lineHeight = parseInt(
+      document.defaultView
+        .getComputedStyle(consoleElem, null)
+        .getPropertyValue("line-height")
+    );
+
+    this.setState({
+      visible_history: Math.floor(clientHeight / lineHeight) - 1,
+    });
+  }
+
+  componentDidMount() {
+    this.computeVisibleHistory();
+
+    fetch("/client.php.txt")
+      .then((response) => response.text())
+      .then((text) => {
+        this.setState({ text: text.split(/\r?\n/) });
+      });
+  }
+
+  focus(e) {
+    e.preventDefault();
+    this.promptRef.current.focus();
+
+    // we type with the mouse at a faster rate to make clicking and keyboard mashing about equal
+    this.typeCode(this.props.typingRate * 5);
+  }
+
+  typeCode(characters) {
+    if (this.state.text.length === 0) return;
+
+    let count = this.state.character_count;
+    let line_step = 1;
+    let line = this.state.line;
+
+    if (count + characters >= this.state.text[line].length) {
+      let remaining = count + characters - this.state.text[line].length;
+      line += line_step;
+
+      while (
+        line < this.state.text.length &&
+        remaining >= this.state.text[line].length
+      ) {
+        remaining -= this.state.text[line].length;
+        line += line_step;
+      }
+
+      this.props.onAddLOC(line - this.state.line);
+
+      if (line >= this.state.text.length) {
+        this.setState({
+          character_count: remaining,
+          line: 0,
+          current_history: this.getHistoryText(0),
+          current_prompt: this.getPromptText(0, remaining),
+        });
+      } else {
+        this.setState({
+          character_count: remaining,
+          line: line,
+          current_history: this.getHistoryText(line),
+          current_prompt: this.getPromptText(line, remaining),
+        });
+      }
+    } else {
+      this.setState({
+        character_count: count + characters,
+        current_prompt: this.getPromptText(line, count + characters),
+      });
+    }
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.typeCode(this.props.typingRate);
+  }
+
+  getPromptText(line, count) {
+    if (this.state.text.length > 0) {
+      let text = this.state.text[line];
+
+      return text.substring(0, count);
+    } else {
+      return "";
+    }
+  }
+
+  getHistoryText(line) {
+    if (this.state.text.length > 0) {
+      let startline =
+        line > this.state.visible_history
+          ? line - this.state.visible_history
+          : 0;
+
+      return this.state.text.slice(startline, line);
+    } else {
+      return [];
+    }
+  }
+
+  render() {
+    return (
+      <div className="Console" onClick={this.focus} ref={this.selfRef}>
+        <div className="History">
+          {this.state.current_history.map((text, i) => (
+            <div key={i}>{text}&nbsp;</div>
+          ))}
+          <div className="Prompt">
+            {this.state.current_prompt}
+            <input
+              ref={this.promptRef}
+              className="Cursor"
+              onChange={this.handleChange}
+              value=""
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Console.propTypes = {
+  onAddLOC: PropTypes.func,
+  typingRate: PropTypes.number,
+};
+
+export default Console;
